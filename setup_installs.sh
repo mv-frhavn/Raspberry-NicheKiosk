df941acd27e472688bc8982980cac3e919e310ac
mv-frhavn <37697785+mv-frhavn@users.noreply.github.com>
2019-04-12 09:53:14 +0200

    Update setup_installs.sh

--- setup_installs.sh
+++ setup_installs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install x11-xserver-utils unclutter -y
+sudo apt-get install xdotool -y
+sudo apt-get update && sudo apt-get upgrade -y
 
