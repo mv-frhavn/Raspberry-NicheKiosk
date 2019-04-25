da809dee81431f4cc0431e5cf393773db3929e79
mv-frhavn <37697785+mv-frhavn@users.noreply.github.com>
2019-04-25 07:43:01 +0200

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i -e 's/@xscreensaver -no-splash/#@xscreensaver -no-splash/' /home/pi/.config/lxsession/LXDE-pi/autostart
-echo @bash /home/pi/kiosk.sh >> /home/pi/.config/lxsession/LXDE-pi/autostart
+sed -i -e 's/@xscreensaver -no-splash/#@xscreensaver -no-splash/' /etc/xdg/lxsession/LXDE-pi/autostart
+echo @bash /home/pi/kiosk.sh >> /etc/xdg/lxsession/LXDE-pi/autostart
 
 mv /home/pi/Raspberry-NicheKiosk/kiosk.sh /home/pi/kiosk.sh
